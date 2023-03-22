03044b0161a5142dfe6fcbd79cad2f146a7a54cb
JVViacrusis <justin.viacrusis21p@gmail.com>
2023-03-21 18:03:51 -0700

    adds Game class skeleton

---
+++ src/main/java/cactus/slabslayer/Game.java
@@ -0,0 +1,37 @@
+package cactus.slabslayer;
+
+/**
+ * Game will have collections of game elements, and game processing that affect game elements.
+ *
+ * @author justin
+ * @version 1.0.1
+ */
+public class Game
+{
+
+  /**
+   * Constructs a new Game object
+   */
+  public Game()
+  {
+
+  }
+
+  /**
+   * Initializes Game object to initial state
+   *
+   * clears all the collections and re-initializes all game processes
+   */
+  public void init()
+  {
+
+  }
+
+  /**
+   * Runs periodically in the processing main loop. Handles timing of all process execution.
+   */
+  public void update()
+  {
+
+  }
+}
