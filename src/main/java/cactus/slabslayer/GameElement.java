3ec380bf66adf4ce1acbe2c0ceec12848e864cf1
katelylynn <katezraly@gmail.com>
2023-03-22 23:58:22 -0700

    Modify GameElement to implement Renderable

--- src/main/java/cactus/slabslayer/GameElement.java
+++ src/main/java/cactus/slabslayer/GameElement.java
@@ -3,7 +3,7 @@ package cactus.slabslayer;
 import processing.core.PVector;
 import processing.data.JSONObject;
 
-public abstract class GameElement implements JSONable {
+public abstract class GameElement implements Renderable, JSONable {
   private PVector position;
   private Window window;
   public GameElement fromJSON(JSONObject json) {
