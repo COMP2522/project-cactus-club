06f17e919cc72f44c750586896080db332382305
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2023-03-25 19:37:36 -0700

    Remove fromJSON method

--- src/main/java/cactus/slabslayer/GameElement.java
+++ src/main/java/cactus/slabslayer/GameElement.java
@@ -6,30 +6,5 @@ import processing.data.JSONObject;
 public abstract class GameElement implements Renderable, JSONable {
   private PVector position;
   private Window window;
-  public GameElement fromJSON(JSONObject json) {
-    // Extract the "type" field from the JSON object
-    String type = json.getString("type");
-
-    // Use a switch statement to create the appropriate game element object
-    switch (type) {
-      case "Paddle":
-        return (GameElement)new Paddle(new Window()).fromJSON(json.toString());
-      case "PowerUp":
-        return (GameElement)new PowerUp().fromJSON(json.toString());
-      case "Ball":
-        return (GameElement)new Ball(new Window()).fromJSON(json.toString());
-      case "Slab":
-        return (GameElement)new Slab().fromJSON(json.toString());
-      case "Layout":
-        return (GameElement)new Layout(new Window()).fromJSON(json.toString());
-      case "Button":
-        return (GameElement)new Button().fromJSON(json.toString());
-      case "Textbox":
-        return (GameElement)new TextBox().fromJSON(json.toString());
-      default:
-        throw new IllegalArgumentException("Unknown game element type: " + type);
-    }
-  }
-
 
 }
