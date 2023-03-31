f726df5fa269d9de57c11addbdbccd21040b594c
shye <113232835+justshye@users.noreply.github.com>
2023-03-31 12:56:02 -0700

    Makes SoundManager have methods to play sounds (usable in collide methods)

--- src/main/java/cactus/slabslayer/SoundManager.java
+++ src/main/java/cactus/slabslayer/SoundManager.java
@@ -1,6 +1,9 @@
 package cactus.slabslayer;
 
 import javax.sound.sampled.*;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * A class that handles playing sounds.
@@ -8,28 +11,74 @@ import javax.sound.sampled.*;
  * @author Trevor
  */
 public class SoundManager {
-  private Clip clip;
+  /**
+   * The background music.
+   */
+  private final Clip bgm;
 
-  public SoundManager(String filePath) {
+  /**
+   * The sound that plays when the ball something.
+   */
+  private final Clip ball;
+
+  /**
+   * The sound that plays when the ball bounces off something.
+   */
+  private final Clip bounce;
+
+  /**
+   * The sound that plays when the paddle hits a powerup.
+   */
+  private final Clip powerup;
+
+
+  public SoundManager() throws FileNotFoundException, LineUnavailableException {
+    Path bgmPath = Paths.get("assets", "audio", "bgm.wav");
+    bgm = loadAudio(bgmPath);
+
+    Path ballPath = Paths.get("assets", "audio", "ball.wav");
+    ball = loadAudio(ballPath);
+
+    Path bouncePath = Paths.get("assets", "audio", "bounce.m4a");
+    bounce = loadAudio(bouncePath);
+
+    Path powerupPath = Paths.get("assets", "audio", "powerup.m4a");
+    powerup = loadAudio(powerupPath);
+  }
+
+  Clip loadAudio(Path path) throws FileNotFoundException, LineUnavailableException {
     try {
-      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream(filePath));
-      clip = AudioSystem.getClip();
+      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(path.toFile());
+      Clip clip = AudioSystem.getClip();
       clip.open(audioInputStream);
+      return clip;
     } catch (Exception e) {
-      e.printStackTrace();
+      throw new FileNotFoundException("Could not find audio file at " + path.toString());
     }
   }
 
-  public void play() {
-    if (clip != null) {
-      clip.setFramePosition(0);
-      clip.start();
+  public void playBGM() {
+    bgm.loop(Clip.LOOP_CONTINUOUSLY);
+  }
+
+  public void playBallSound() {
+    if (ball != null) {
+      ball.setFramePosition(0);
+      ball.start();
     }
   }
 
-  public void stop() {
-    if (clip != null) {
-      clip.stop();
+  public void playBounceSound() {
+    if (bounce != null) {
+      bounce.setFramePosition(0);
+      bounce.start();
+    }
+  }
+
+  public void playPowerupSound() {
+    if (powerup != null) {
+      powerup.setFramePosition(0);
+      powerup.start();
     }
   }
 }
