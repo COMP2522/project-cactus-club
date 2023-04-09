12da92e6c1a9d44633277b42d9bb683cd2eb2d37
CyrusVillaruz <91701334+CyrusVillaruz@users.noreply.github.com>
2023-04-09 13:34:22 -0700

    Adds JavaDoc to Collidable interface

--- src/main/java/cactus/slabslayer/Collidable.java
+++ src/main/java/cactus/slabslayer/Collidable.java
@@ -1,9 +1,11 @@
 package cactus.slabslayer;
 
+/**
+ * An interface containing collision methods between objects.
+ */
 public interface Collidable {
+  boolean isCollidingWith(Object toCheck);
 
-    boolean isCollidingWith(Object toCheck);
-
-    void doCollision(Object collidedWith);
+  void doCollision(Object collidedWith);
 
 }
